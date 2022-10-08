f479bbc25d7cd5e91396973a1c41c45e1d71c6fb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-08 20:52:19 +0000

    Adding tarfile member sanitization to extractall()

--- main.py
+++ main.py
@@ -34,7 +34,26 @@ def init(quiet=False):
     print("Downloading...")
     asyncio.run(download_both())
     with tarfile.open("server/java.tar.gz", "r:gz") as arc:
-        arc.extractall("server/java")
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(arc, "server/java")
 
     # Finds Java directory
     java_dir = os.listdir("server/java")[0] + "/bin/java"
